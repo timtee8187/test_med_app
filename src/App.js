086da31cf39832ed71b76d164f082859c29431d7
timtee8187 <ezeamara212@gmail.com>
2025-03-23 20:54:40 -0400

    Navbar and Landing updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary modules from React library
+import React from 'react';
 
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import custom Navbar component
+import Navbar from './Components/Navbar/Navbar';
+
+// Import LandingPage1 component
+import LandingPage1 from './components/LandingPage1/LandingPage1';
+
+// Function component for the main App
 function App() {
+  // Render the main App component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Set up BrowserRouter for routing */}
+      <BrowserRouter>
+        {/* Display the Navbar component */}
+        <Navbar />
+
+        {/* Set up the Routes for different pages */}
+        <Routes>
+          {/* Define individual Route components for different pages */}
+          <Route path="/" element={<LandingPage1 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
+// Export the App component as the default export
 export default App;
