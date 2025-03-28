1935f333cd616eafaab16ea43e8a4ab46bc20936
timtee8187 <ezeamara212@gmail.com>
2025-03-27 20:25:55 -0400

    Implemented auth-aware Navbar with logout functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Changed from <a> to <Link>
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
 
-  const handleClick = () => {
-    setIsMenuOpen(!isMenuOpen);
+  useEffect(() => {
+    const userData = JSON.parse(sessionStorage.getItem('userData'));
+    if (userData) {
+      setIsLoggedIn(true);
+      setUsername(userData.email.split('@')[0]);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    sessionStorage.clear();
+    setIsLoggedIn(false);
+    navigate('/login');
   };
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <nav className="navbar-container">
+    <nav>
       <div className="nav__logo">
-        <Link to="/"> {/* Changed to React Router Link */}
+        <Link to="/">
           StayHealthy
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -21,35 +36,51 @@ function Navbar() {
             viewBox="0 0 1000 1000"
             style={{ fill: '#3685fb' }}
           >
-            {/* ... keep your existing SVG ... */}
+            {/* Your SVG */}
           </svg>
         </Link>
         <span>.</span>
       </div>
 
-      <div className="nav__icon" onClick={handleClick}>
+      <div className="nav__icon" onClick={toggleMenu}>
         <i className={`fa ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
       </div>
 
       <ul className={`nav__links ${isMenuOpen ? 'active' : ''}`}>
         <li className="link">
-          <Link to="/">Home</Link> {/* Updated to React Router */}
+          <Link to="/">Home</Link>
         </li>
         <li className="link">
           <Link to="/appointments">Appointments</Link>
         </li>
         <li className="link">
-          <Link to="/health blogs">Health Blogs</Link>
+          <Link to="/health-blogs">Health Blogs</Link>
         </li>
         <li className="link">
           <Link to="/reviews">Reviews</Link>
         </li>
-        <li className="link">
-          <Link to="/signup" className="btn1">Sign Up</Link>
-        </li>
-        <li className="link">
-          <Link to="/login" className="btn1">Login</Link>
-        </li>
+
+        {isLoggedIn ? (
+          <li className="welcome-user">
+            <span>Welcome, {username}</span>
+            <ul className="dropdown-menu">
+              <li>
+                <button onClick={handleLogout} className="btn2">
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </li>
+        ) : (
+          <>
+            <li className="link">
+              <Link to="/signup" className="btn1">Sign Up</Link>
+            </li>
+            <li className="link">
+              <Link to="/login" className="btn1">Login</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
